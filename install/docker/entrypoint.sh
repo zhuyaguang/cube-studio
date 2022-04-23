722f03a5c5a6dc29216597bd69361a324bde9907
pengluan <pengluan@tencent.com>
2022-04-23 21:38:27 +0800

    fix

--- install/docker/entrypoint.sh
+++ install/docker/entrypoint.sh
@@ -10,12 +10,13 @@ ln -s /data/k8s/kubeflow/global/static /home/myapp/myapp/static/appbuilder/mnt
 
 if [ "$STAGE" = "init" ]; then
   export FLASK_APP=myapp:app
+  python create_db.py
   myapp fab create-admin --username admin --firstname admin --lastname admin --email admin@tencent.com --password admin
   # myapp db init    # 生成migrations文件夹
   # myapp db migrate   # 生成对应版本数据库表的升级文件到versions文件夹下，需要你的数据库是已经upgrade的
   myapp db upgrade   # 数据库表同步更新到mysql
   # 会创建默认的角色和权限。会创建自定义的menu权限，也才能显示自定义menu。
-  myapp init_db
+  mysql -uroot -padmin -e "select now()" -h127.0.0.1 -p3306
   myapp init
 
 elif [ "$STAGE" = "build" ]; then
