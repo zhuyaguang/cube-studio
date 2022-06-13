b73da786e8883715c6a0ca95151f015a0f55fbee
pengluan <pengluan@tencent.com>
2022-06-13 10:17:44 +0800

    fix

--- job-template/job/horovod/build.sh
+++ job-template/job/horovod/build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 set -ex
-docker build -t csighub.tencentyun.com/tme-kubeflow/horovod:cpu-20210401 -f job/horovod/Dockerfile-cpu .
-docker push csighub.tencentyun.com/tme-kubeflow/horovod:cpu-20210401
+docker build -t ccr.ccs.tencentyun.com/cube-studio/horovod:20210401 -f job/horovod/Dockerfile .
+docker push ccr.ccs.tencentyun.com/cube-studio/horovod:20210401
 
-#docker build -t csighub.tencentyun.com/tme-kubeflow/horovod:gpu-20210401 -f job/horovod/Dockerfile-gpu .
-#docker push csighub.tencentyun.com/tme-kubeflow/horovod:gpu-20210401
 
 
 
